3298c7b62fd044781a9c637b282dc9d5d96da496
danielcsmit <100503051+danielcsmit@users.noreply.github.com>
2022-08-14 20:25:45 +0700

    Revise cleaning script after interpretting requirements differently

--- run_analysis.R
+++ run_analysis.R
@@ -6,29 +6,69 @@ unzip('humanActivityRecognition.zip', exdir='.')
 # Load data
 xTest <- read.table('UCI HAR Dataset/test/X_test.txt', header = FALSE, sep = '\n', dec = '.')
 yTest <- read.table('UCI HAR Dataset/test/y_test.txt', header = FALSE, sep = '\n', dec = '.')
+subjectTest <- read.table('UCI HAR Dataset/test/subject_test.txt', header = FALSE, sep = '\n', dec = '.')
 xTrain <- read.table('UCI HAR Dataset/train/X_train.txt', header = FALSE, sep = '\n', dec = '.')
 yTrain <- read.table('UCI HAR Dataset/train/y_train.txt', header = FALSE, sep = '\n', dec = '.')
+subjectTrain <- read.table('UCI HAR Dataset/train/subject_train.txt', header = FALSE, sep = '\n', dec = '.')
 
 # Merge Test data
 xJoinedData <- rbind(xTest, xTrain)
 yData <- rbind(yTest, yTrain)
-rm(list=c('xTest', 'yTest', 'xTrain', 'yTrain'))
+subjectData <- rbind(subjectTest, subjectTrain)
+rm(list=c('xTest', 'yTest', 'subjectTest', 'xTrain', 'yTrain', 'subjectTrain'))
+
+# Rename y data column name
+colnames(yData) <- c('Activity')
+
+# Add labels for y data
+library(dplyr)
+yData <- yData %>%
+  mutate(ActivityLabel = case_when(
+    Activity == 1 ~ 'WALKING',
+    Activity == 2 ~ 'WALKING_UPSTAIRS',
+    Activity == 3 ~ 'WALKING_DOWNSTAIRS',
+    Activity == 4 ~ 'SITTING',
+    Activity == 5 ~ 'STANDING',
+    Activity == 6 ~ 'LAYING',
+  ))
 
 # Load feature names and replace special characters with dashes
 featureNames <- read.table('UCI HAR Dataset/features.txt', header = FALSE, sep = '\n')
 
 # Separate xJoinedData features into columns and label using feature names
 xData <- read.table(text = xJoinedData$V1, header = FALSE, strip.white = TRUE, col.names=featureNames$V1)
+
+# Extract only mean and standard deviation values
+xData <- xData[, c(1,2,3,4,5,6,
+    41,42,43,44,45,46,
+    81,82,83,84,85,86,
+    121,122,123,124,125,126,
+    161,162,163,164,165,166,
+    201,202,
+    214,215,
+    227,228,
+    240,241,
+    253,254,
+    266,267,268,269,270,271,
+    294,295,296,
+    345,346,347,348,349,350,
+    373,374,375,
+    424,425,426,427,428,429,
+    452,453,454,
+    503,504,
+    513,
+    516,517,
+    526,
+    529,530,
+    539,
+    542,543,
+    552,
+    555,556,557,558,559,560,561)]
 rm(list=c('xJoinedData', 'featureNames'))
 
-# Determine mean and standard deviation of measurements
-xMeans <- apply(xData, 2, mean)
-xStdDev <- apply(xData, 2, sd)
-
-# Merge means and standard deviations into a single data frame
-xSummary <- data.frame(xMeans, xStdDev)
-rm(list=c('xMeans', 'xStdDev'))
+# Merge Y data and Subject tables with X data
+combinedData <- cbind(yData, xData)
 
 # Export summary dataset as txt file
-write.table(xSummary, 'x_summary.txt', append = FALSE, sep = " ", dec = ".",
+write.table(combinedData, 'tidyData.txt', append = FALSE, sep = " ", dec = ".",
             row.names = FALSE, col.names = TRUE)
